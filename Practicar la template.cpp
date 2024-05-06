ade258355c824a1c21f0bd67675366148ac4abce
Blackcronw <124933704+Blackcronw@users.noreply.github.com>
2024-05-06 10:11:30 -0600

    Add files via upload

---
+++ Practicar la template.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ENDL '\n'
+#define f first 
+#define s second 
+#define fore(i,a,b) for(lli i=(a), ThxMK = (b); i<ThxMK;++i)
+#define pb push_back
+#define all(s) begin(s),end(s)
+#define sz(s) int(s.size())
+#define deb(x) cout<< #x ": " << (x) << ENDL;
+typedef long long lli;
+typedef long double ld;
+typedef pair<lli,lli>ii;
+typedef vector<lli>vi;  
+
+void solve(){
+    
+}
+
+int main(){_
+
+    return 0;
+}
+
